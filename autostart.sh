84c1e06053db14addad37b11579ae1fee3e58aa4
hellopoisonx <x1665341912@gmail.com>
2023-05-11 20:27:03 +0800

    dwm_default_bar => lemonbar

--- autostart.sh
+++ autostart.sh
@@ -2,7 +2,6 @@
 fcitx5 -d
 picom --experimental-backend --config ~/.config/picom.conf -b
 python ~/dwm/script/background.py
-~/lemonbar/__init__.py
 diskie &
 dunst &
 pulseaudio -k ; sleep 2s ;pulseaudio --start

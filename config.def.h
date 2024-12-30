e21969dbd3852046e40081aa75c2f438222c213b
hellopoisonx <x1665341912@gmail.com>
2024-12-30 21:15:31 +0800

    fix window title(bar) color

--- config.def.h
+++ config.def.h
@@ -21,8 +21,9 @@ static const char *colors[][3]      = {
 };
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border*/
-    [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+    [SchemeNorm]   = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]    = { OPAQUE, baralpha, borderalpha },
+	[SchemeTitle]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
